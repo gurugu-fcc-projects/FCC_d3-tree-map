94817bba7b6df68e33028a478d5ca2835cc2d401
GuRuGu <peter.k.nets@gmail.com>
2021-06-03 17:14:43 +0300

    Load data

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 //--> Main data source
 const url =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+
+//--> Load & display data
+d3.json(url).then(data => {
+  console.log(data);
+});
