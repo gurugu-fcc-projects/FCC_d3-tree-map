d7ce2eea260d444480af260b3e7cd03ff908885b
GuRuGu <peter.k.nets@gmail.com>
2021-06-03 17:28:32 +0300

    Create an svg element

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const url =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
 
+//--> Constants
+const width = 1000;
+const height = 800;
+
+const svg = d3
+  .select(".content")
+  .append("svg")
+  .attr("viewbox", [0, 0, width, height]);
+
 //--> Load & display data
 d3.json(url).then(data => {
   console.log(data);
